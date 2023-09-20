b5e8f846c777f61348eecb5bb3cc7240ed26a7d5
olixc <noibiabdulrasaq@rocketmail.com>
2023-09-20 02:48:54 +0100

    Modified main.tf file

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  config_context = "kind-my-cluster"
+  config_context = "kind-my-cluster" # Please change this to your context e.g. kind-<cluster-name>
 }
